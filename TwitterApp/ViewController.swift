3b5ec739293123b004e7a87c4f3e644d0c93f7e0
kenneth moody <km3372@gmail.com>
2019-05-14 13:00:19 -0400

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by kenneth moody on 5/14/19.
+//  Copyright © 2019 Kenneth Moody Sr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
